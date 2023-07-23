ac3df5a4433ea696ed6a7110cf50105f10ed16f3
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-07-24 02:14:43 +0530

    Achievers Page Done With Font and Color

--- client/src/app/achievers/SingleAchiver.js
+++ client/src/app/achievers/SingleAchiver.js
@@ -6,10 +6,28 @@ export default function SingleAchiever(props) {
     <div className='achiever-single-story-main'>
       <div className="single-story-container">
         <div className="achiever-image-container">
-          <img src={props.achieverimage} alt="" />
+          <img src={props.achiever_image} alt="" />
         </div>
         <div className="achiever-content-container">
-          achiever
+          <div className="achiever-name-container">
+            {props.achiever_name}
+          </div>
+          <div className="achiever-designation">
+            {props.achiver_designation}
+          </div>
+          <div className="achiever-story">
+            <p>{props.achiever_story}</p>
+          </div>
+          <div className="achiever-more">
+            <div className="connect-to-achiever">
+              <a href={props.connect}>Connect</a>
+              {/* <img src="https://cdn-icons-png.flaticon.com/512/3128/3128219.png" alt="" /> */}
+              <img src="https://cdn-icons-png.flaticon.com/512/3536/3536569.png" alt="" />
+            </div>
+            <div className="readmore-achiever">
+              <button>Read More</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- client/src/app/achievers/page.js
+++ client/src/app/achievers/page.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import SingleAchiever from './SingleAchiver'
-// import './Achievers.css'
+import './Achievers.css'
 
 export default function page() {
   return (
     <div className='achievers-main'>
       <div className="achievers-section-1-container">
-        <SingleAchiever achieverimage={'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600'} />
+        <SingleAchiever achiever_image={'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600'}
+        achiever_name={'Priyanka Sharma'} achiver_designation={'Project Lead, XtrimCoder'}
+        achiever_story={'The sun is a huge ball of gases. It has a diameter of 1,392,000 km. It is so huge that it can hold millions of planets inside it. The Sun is mainly made up of hydrogen and helium gas.The surface of the Sun is known as the photosphere. The photosphere is surrounded by a thin layer of gas known as the chromospheres. Without the Sun, there would be no life on Earth.There would be no plants, no animals and no human beings. As, all the living things on Earth get their energy from the Sun for their survival.....'}
+        connect={'https://www.instagram.com/taaarannn/'} />
+        
+        <SingleAchiever achiever_image={'https://images.pexels.com/photos/17117578/pexels-photo-17117578/free-photo-of-profile-of-a-young-woman-wearing-sunglasses.jpeg?auto=compress&cs=tinysrgb&w=600'}
+        achiever_name={'Priyanka Sharma'} achiver_designation={'Project Lead, XtrimCoder'} 
+        achiever_story={'The sun is a huge ball of gases. It has a diameter of 1,392,000 km. It is so huge that it can hold millions of planets inside it. The Sun is mainly made up of hydrogen and helium gas.'}
+        connect={'https://www.instagram.com/taaarannn/'} />
+        
+        <SingleAchiever achiever_image={'https://images.pexels.com/photos/4919373/pexels-photo-4919373.jpeg?auto=compress&cs=tinysrgb&w=600'}
+        achiever_name={'Priyanka Sharma'} achiver_designation={'Project Lead, XtrimCoder'}
+        achiever_story={'The sun is a huge ball of gases. It has a diameter of 1,392,000 km. It is so huge that it can hold millions of planets inside it. The Sun is mainly made up of hydrogen and helium gas.'}
+        connect={'https://www.instagram.com/taaarannn/'} />
       </div>
     </div>
   )
