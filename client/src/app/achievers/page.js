63d54a6a1558d73a92f64ed644178e1c7b44c6e9
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-07-23 19:31:01 +0530

    Coloring the Navbar No:3

--- client/src/app/achievers/page.js
+++ client/src/app/achievers/page.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import './Achievers.css'
+import SingleAchiever from './SingleAchiver'
+// import './Achievers.css'
+
 export default function page() {
-    return (
-        <div className='achievers-main'>
-            <div className="achievers-section-1-container">
-                Achievers page
-            </div>
-        </div>
-    )
+  return (
+    <div className='achievers-main'>
+      <div className="achievers-section-1-container">
+        <SingleAchiever achieverimage={'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600'} />
+      </div>
+    </div>
+  )
 }
