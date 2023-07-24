43f4da3f4caf8d5aca24ec05e47e7132747b3d28
sharma39vishal <sharma39vishal@gmail.com>
2023-07-24 13:45:37 +0530

    Api integration

--- Routes/AchiversRoutes.js
+++ Routes/AchiversRoutes.js
@@ -21,18 +21,18 @@ router.get("/:id",(req, res) => {
 });
 
 //Route to add a new post
-router.post("/addachivers",isauthenticated,(req, res) => {
-    //Retrieve data for post
-    const {images,user_id,Title,discription,tags} = req.body;
-    //Create a new Post and save it to DB
-    // console.log(req.body);
-    const newAchivers = new Achivers({images,user_id,Title,discription,Comments:[],otherdetails:[],tags,created_at:new Date(),updated_at:new Date()});
-    // Save the new post
-    Recordlog({user_id,ip:req.ip,what:"New Achivers Added"})
-    newAchivers.save()
-        .then(() => res.json("Achivers Added!"))
-        .catch((err) => res.status(400).json("Error: " + err));
-});
+// router.post("/addachivers",(req, res) => {
+//     //Retrieve data for post
+//     const {images,name,designation,discription,connect} = req.body;
+//     //Create a new Post and save it to DB
+//     // console.log(req.body);
+//     const newAchivers = new Achivers({images,name,designation,discription,Comments:[],otherdetails:[],connect,tags:[],created_at:new Date(),updated_at:new Date()});
+//     // Save the new post
+//     Recordlog({user_id:"Admin",ip:req.ip,what:"New Achivers Added"})
+//     newAchivers.save()
+//         .then(() => res.json("Achivers Added!"))
+//         .catch((err) => res.status(400).json("Error: " + err));
+// });
 
 
 module.exports = router;
--- client/src/app/achievers/page.js
+++ client/src/app/achievers/page.js
@@ -1,26 +1,40 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import SingleAchiever from './SingleAchiver'
 import './Achievers.css'
+import axios from 'axios';
+
+
 
 export default function page() {
+  const [content, setcontent] = useState([]);
+
+  const callapi=async ()=>{
+    axios.get("http://localhost:5000/achiver/")
+    .then((res)=>{
+      setcontent(res.data);
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+  useEffect(() => {
+    callapi();
+  },[])
+  
+
   return (
     <div className='achievers-main'>
       <div className="achievers-section-1-container">
-        <SingleAchiever achiever_image={'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=600'}
-        achiever_name={'Priyanka Sharma'} achiver_designation={'Project Lead, XtrimCoder'}
-        achiever_story={'The sun is a huge ball of gases. It has a diameter of 1,392,000 km. It is so huge that it can hold millions of planets inside it. The Sun is mainly made up of hydrogen and helium gas.The surface of the Sun is known as the photosphere. The photosphere is surrounded by a thin layer of gas known as the chromospheres. Without the Sun, there would be no life on Earth.There would be no plants, no animals and no human beings. As, all the living things on Earth get their energy from the Sun for their survival.....'}
-        connect={'https://www.instagram.com/taaarannn/'} />
-        
-        <SingleAchiever achiever_image={'https://images.pexels.com/photos/17117578/pexels-photo-17117578/free-photo-of-profile-of-a-young-woman-wearing-sunglasses.jpeg?auto=compress&cs=tinysrgb&w=600'}
-        achiever_name={'Priyanka Sharma'} achiver_designation={'Project Lead, XtrimCoder'} 
-        achiever_story={'The sun is a huge ball of gases. It has a diameter of 1,392,000 km. It is so huge that it can hold millions of planets inside it. The Sun is mainly made up of hydrogen and helium gas.'}
-        connect={'https://www.instagram.com/taaarannn/'} />
-        
-        <SingleAchiever achiever_image={'https://images.pexels.com/photos/4919373/pexels-photo-4919373.jpeg?auto=compress&cs=tinysrgb&w=600'}
-        achiever_name={'Priyanka Sharma'} achiver_designation={'Project Lead, XtrimCoder'}
-        achiever_story={'The sun is a huge ball of gases. It has a diameter of 1,392,000 km. It is so huge that it can hold millions of planets inside it. The Sun is mainly made up of hydrogen and helium gas.'}
-        connect={'https://www.instagram.com/taaarannn/'} />
+        {content.map((item)=>{
+         return <SingleAchiever achiever_image={item.images}
+          achiever_name={item.name} achiver_designation={item.designation}
+          achiever_story={item.discription}
+          connect={item.connect} />
+        })}
       </div>
     </div>
   )
 }
+
+
