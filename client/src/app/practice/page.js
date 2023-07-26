4081c0211f594a2aa8ccc34dcef3d7f47069ae67
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-07-26 21:43:51 +0530

    Dsa Sheet Done

--- client/src/app/practice/page.js
+++ client/src/app/practice/page.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import './Practice.css'
+import DsaSheet from './DsaSheet'
+
 export default function page() {
-    return (
-        <div className='practice-main'>
-            <div className="practice-section-1-container">
-                Practice page
-            </div>
-        </div>
-    )
+  return (
+    <div className='practice-main'>
+      <div className="practice-dsa-sheet-container">
+        <DsaSheet dsasheet_name={'Love Babbar'} />
+        <DsaSheet dsasheet_name={'Striver'} />
+        <DsaSheet dsasheet_name={'Code With Harry'} />
+        <DsaSheet dsasheet_name={'Virat Kohli'} />
+        <DsaSheet dsasheet_name={'Rohit Sharma'} />
+        <DsaSheet dsasheet_name={'Nehanshu Ajmera'} />
+        <DsaSheet dsasheet_name={'Nehanshu Ajmera'} />
+      </div>
+    </div>
+  )
 }
