f5bfa46ea8598042ae2eb83936b108a6b6880ec8
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-07-27 20:43:28 +0530

    New Dsa Sheet 2.o And Core Subjects

--- client/src/app/practice/DsaSheet.js
+++ client/src/app/practice/DsaSheet.js
@@ -3,14 +3,8 @@ import './DsaSheet.css'
 export default function SingleSheet(props) {
   return (
     <div className="dsa-sheet-main">
-        <div className="dsapractice-sheet-layout">
-            <div className="dsa-sheet-name">
-              {props.dsasheet_name}
-            </div>
-            {/* <div className="start-dsa-practice">
-              <h5>{props.start_learning}</h5>
-            </div> */}
-        </div>
+      <img src={props.creatorImage} alt="Loading Error" />
+      <h4>{props.dsasheet_name}</h4>
     </div>
   )
 }
--- client/src/app/practice/page.js
+++ client/src/app/practice/page.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import './Practice.css'
 import DsaSheet from './DsaSheet'
+import CoreSubject from './CoreSubject'
 
 export default function page() {
   return (
     <div className='practice-main'>
-      <div className="practice-dsa-sheet-container">
-        <DsaSheet dsasheet_name={'Love Babbar'} />
-        <DsaSheet dsasheet_name={'Striver'} />
-        <DsaSheet dsasheet_name={'Code With Harry'} />
-        <DsaSheet dsasheet_name={'Virat Kohli'} />
-        <DsaSheet dsasheet_name={'Rohit Sharma'} />
-        <DsaSheet dsasheet_name={'Nehanshu Ajmera'} />
-        <DsaSheet dsasheet_name={'Nehanshu Ajmera'} />
+      <div className="practice-dsa-sheet">
+        <div className="dsa-sheet-heading">
+          <h4>DSA Sheet</h4>
+          <h5>See All</h5>
+        </div>
+        <div className="practice-dsa-sheet-container">
+          <DsaSheet creatorImage={'https://media.licdn.com/dms/image/C4D22AQGIXuAu2GS6rQ/feedshare-shrink_800/0/1673000670313?e=1693440000&v=beta&t=ztxyvEpfNokyoXnXC6itQxrsdR7usq_PAFRxEKwsFOE'} dsasheet_name={'Love Babbar'} />
+          <DsaSheet creatorImage={'https://media.licdn.com/dms/image/C4D03AQGFTnOaQa4fUQ/profile-displayphoto-shrink_400_400/0/1623400653670?e=1695859200&v=beta&t=Qjonidgbofugle-MTZxZK3StOG6Mo_fO9QZjkkULzNA'} dsasheet_name={'Striver'} />
+          <DsaSheet creatorImage={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWkJ8124cXJ5TXCEWZJD--sWMYCknz7JJKHw&usqp=CAU'} dsasheet_name={'Code With Harry'} />
+          <DsaSheet creatorImage={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQr-w4OwyiYM1Oypztaz8wzLJnPJhMdb9WMVw&usqp=CAU'} dsasheet_name={'Virat Kohli'} />
+          <DsaSheet creatorImage={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDmrIcFwYig92o99zR_9ethhckL0wBVV_64w&usqp=CAU'} dsasheet_name={'Rohit Sharma'} />
+        </div>
+      </div>
+      <div className="practice-core-subject-container">
+        <CoreSubject subjectName={'Data Structure And Algorithms'} />
+        <CoreSubject subjectName={'Operating System'} />
+        <CoreSubject subjectName={'Database Management System'} />
+        <CoreSubject subjectName={'Object Oriented Programming'} />
+        <CoreSubject subjectName={'Computer Networks'} />
       </div>
     </div>
   )
