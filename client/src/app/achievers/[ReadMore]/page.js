9fd7900bf7a39088e779d4a708643b5bd7786879
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-08-06 13:54:24 +0530

    Single page

--- client/src/app/achievers/[ReadMore]/page.js
+++ client/src/app/achievers/[ReadMore]/page.js
@@ -2,22 +2,37 @@
 import axios from 'axios'
 import './readMore.css'
 import React, { useEffect, useState } from 'react'
+import Link from 'next/link';
 
 export default function page({ params }) {
   const [readMoreData, setreadMoreData] = useState(null);
   useEffect(() => {
-  axios.get(`/achiver/${params.ReadMore}`)
-  .then((res) => {
-    setreadMoreData(res.data);
-    console.log(res.data);
-  })
-  
+    axios.get(`/achiver/${params.ReadMore}`)
+      .then((res) => {
+        setreadMoreData(res.data);
+        console.log(res.data);
+      })
+
   }, []);
-  
+
   return (
     <div className='achiever-read-more-page'>
-      <div className="name-of-achiever">
-        <h1>{readMoreData?.name}</h1>
+      <div className="selected-achiever-page-container">
+        <div className="achiever-fixed-section-container">
+          <div className="achiever-fixed-image">
+            <img src={readMoreData?.images} alt="" />
+          </div>
+          <div className="achiever-fixed-description">
+            <h2>{readMoreData?.name}</h2>
+            <h4>{readMoreData?.designation}</h4>
+          </div>
+          <div className="achiever-fixed-connect">
+            <a href={readMoreData?.connect} target="_blank">Connect</a>
+          </div>
+        </div>
+        <div className="achiever-dynamic-section-container">
+          <p>{readMoreData?.discription}</p>
+        </div>
       </div>
     </div>
   )
