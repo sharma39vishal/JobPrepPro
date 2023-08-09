dd36e44fe302f20e30d30a668bd01c376f25b49d
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-08-09 21:18:46 +0530

    Single Achiever Complete

--- client/src/app/achievers/[ReadMore]/page.js
+++ client/src/app/achievers/[ReadMore]/page.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 import './readMore.css'
 import React, { useEffect, useState } from 'react'
-import Link from 'next/link';
+// import Link from 'next/link';
 
 export default function page({ params }) {
   const [readMoreData, setreadMoreData] = useState(null);
@@ -18,6 +18,7 @@ export default function page({ params }) {
   return (
     <div className='achiever-read-more-page'>
       <div className="selected-achiever-page-container">
+        <div className="before-fixed-achiever"></div>
         <div className="achiever-fixed-section-container">
           <div className="achiever-fixed-image">
             <img src={readMoreData?.images} alt="" />
@@ -27,11 +28,14 @@ export default function page({ params }) {
             <h4>{readMoreData?.designation}</h4>
           </div>
           <div className="achiever-fixed-connect">
-            <a href={readMoreData?.connect} target="_blank">Connect</a>
+            <a href={readMoreData?.connect} target="_blank">linked</a>
+            <img src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" alt="" />
           </div>
         </div>
         <div className="achiever-dynamic-section-container">
+          <div className="fixed-inside-dynamic-achiever-before"></div>
           <p>{readMoreData?.discription}</p>
+          <div className="fixed-inside-dynamic-achiever-after"></div>
         </div>
       </div>
     </div>
