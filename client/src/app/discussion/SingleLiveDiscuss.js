98f3ce916ba26751fe43d28a21b773f9f2be2b29
Nehanshu Ajmera <nehanshuajmera@gmail.com>
2023-08-11 16:52:06 +0530

    Live Discussion Complete

--- client/src/app/discussion/SingleLiveDiscuss.js
+++ client/src/app/discussion/SingleLiveDiscuss.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Discussion.css'
+import Link from 'next/link'
 export default function SingleLiveDiscuss(props) {
   return (
     <div onClick={() => {
       props.setimage(props.discussImage)
     }} className="live-discussion-description">
-      <h3>{props.discussDescription}</h3>
+      <Link href=''><h3>{props.discussDescription}</h3></Link> 
     </div>
   )
 }
--- client/src/app/discussion/page.js
+++ client/src/app/discussion/page.js
@@ -24,13 +24,14 @@ export default function page() {
   return (
     <div className='discussion-main'>
       <div className="discussion-section-1-container">
+        <h2>Click below links to join Live Discussion</h2>
         <div className="live-single-discuss-container">
           <div className='discussion-description'>
             <SingleLiveDiscuss setimage={setimage} discussDescription='Database Management System' discussImage='\images\dbmsDiscuss.png' />
-            <SingleLiveDiscuss setimage={setimage} discussDescription='DSA Live Discussion' discussImage='\images\dsa-discuss.jpg' />
-            <SingleLiveDiscuss setimage={setimage} discussDescription='CN Live Discussion' discussImage='\images\networkDiscuss.png' />
-            <SingleLiveDiscuss setimage={setimage} discussDescription='OS Live Discussion' discussImage='\images\OsDiscuss.jpg' />
-            <SingleLiveDiscuss setimage={setimage} discussDescription='OOPS Live Discussion' discussImage='\images\discussOops.png' />
+            <SingleLiveDiscuss setimage={setimage} discussDescription='Data Structure And Algo.' discussImage='\images\dsa-discuss.jpg' />
+            <SingleLiveDiscuss setimage={setimage} discussDescription='Computer Networks' discussImage='\images\networkDiscuss.png' />
+            <SingleLiveDiscuss setimage={setimage} discussDescription='Operating Systems' discussImage='\images\OsDiscuss.jpg' />
+            <SingleLiveDiscuss setimage={setimage} discussDescription='Object Oriented Programming' discussImage='\images\discussOops.png' />
           </div>
           <div className="live-discussion-image">
             <img src={image} alt="Loading Error" />
